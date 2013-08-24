72923771877a297807d62f945314cf00470e7f32
Dmitriy Galnykin <developer085@gmail.com>
2013-08-24 20:38:54 +0400

    wmflow
    start

---
+++ js/ready.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+var container = document.querySelector('#container');
+var msnry;
+// initialize Masonry after all images have loaded
+imagesLoaded( container, function() {
+  msnry = new Masonry( container, {
+  // options
+  
+  "isFitWidth": true,
+  itemSelector: '.item'
+} );
+});
+
+
+
+});
+
